f06cbbd5b2b2df3955723cba20901ad26e623b5f
WahidX <wahidx93@gmail.com>
2024-01-30 21:57:43 +0530

    added error color

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,58 +1,63 @@
 import React, { useEffect, useState } from 'react';
-import { generate, count } from "random-words";
+import { generate } from "random-words";
 
-const Content = ({SelectedTimer,wheninput,result,data,color}) => {
+const Content = ({ SelectedTimer, wheninput, result, data, color }) => {
   const [content, setContent] = useState([]);
-  const [isfocus,setFocus] = useState(true);
-  let [translate,setTranslate] = useState(0);
-  let [translate2,setTranslate2] = useState(0);
-  
+  const [userInput, setUserInput] = useState('');
+  const [isCorrect, setIsCorrect] = useState(true);
+  const [isFocus, setFocus] = useState(true);
+  let [translate, setTranslate] = useState(0);
+  let [translate2, setTranslate2] = useState(0); 
+
   useEffect(() => {
-	setContent(generate(SelectedTimer*5));
-	
+    setContent(generate(SelectedTimer * 5));
   }, [SelectedTimer]);
-  useEffect(()=>{
-	data(content);
-  },[content])  
+
   useEffect(() => {
-	document.getElementById("customcursor").style.transform=`translate(${translate}px,${translate2}px)`
-  },[translate,translate2]);
-  const masti= (e) =>{
+    data(content);
+  }, [content]);
+
+  useEffect(() => {
+    document.getElementById("customcursor").style.transform = `translate(${translate}px,${translate2}px)`;
+  }, [translate, translate2]);
+
+
+  useEffect(()=>{
+	const enteredText = userInput.toLowerCase();
+	const generatedText = content.join(' ').toLowerCase();
+	setIsCorrect(enteredText[enteredText.length - 1]=== generatedText[enteredText.length - 1]);
+  },[userInput]);
+  const masti = (e) => {
+    setUserInput(e.target.value);
 	result(e.target.value);
-	setTranslate(prevtrans =>{
-		return prevtrans+15;
-	});
-  }
-	
-  
-	
+    setTranslate(prevtrans => prevtrans + 15);
+  };
+
   return (
     <div id="content">
-      {content.map((word,index)=>{
-		return(
-			<div key={index}> {word}</div>
-		)
-	  })}
+      {content.map((word, index) => (
+        <span key={index} style={{ color: isCorrect ? 'grey' : 'red' }}>
+          {word}
+        </span>
+      ))}
 
-	  <textarea autoFocus={isfocus} onFocus={()=>{setFocus(true)}} onBlur={()=>{setFocus(false)}} id="usercontent" onInput={() => wheninput()} onKeyDown={(e)=>{
-		if (e.key==="Backspace"){
-			setTranslate(prevtrans =>{
-                return prevtrans<=0? 0 : prevtrans-30
-            });
-		}
-		else if(e.key==="Space"){
-			setTranslate(prevtrans =>{
-				return prevtrans-3
-			})
-		}
-		else if(e.key==="Enter"){
-			setTranslate2(prevtrans2 =>{
-				return prevtrans2+30;
-			})
-			setTranslate(-15);
-		}
-	  }} onChange={(e)=>{masti(e)}}></textarea>
-	  <div id="customcursor" style={{visibility:isfocus?"visible":"hidden"}}></div>
+      <textarea
+        autoFocus={isFocus} onFocus={() => setFocus(true)} onBlur={() => setFocus(false)} id="usercontent" onInput={() => wheninput()}
+        onKeyDown={(e) => {
+          if (e.key === 'Backspace') {
+            setTranslate(prevtrans => (prevtrans <= 0 ? 0 : prevtrans - 30));
+          } else if (e.key === 'Space') {
+            setTranslate((prevtrans) => prevtrans - 3);
+          } else if (e.key === 'Enter') {
+            setTranslate2((prevtrans2) => prevtrans2 + 30);
+            setTranslate(-15);
+          }
+        }}
+        onChange={(e) => {
+          masti(e);
+        }}
+      ></textarea>
+      <div id="customcursor" style={{ visibility: isFocus ? 'visible' : 'hidden' }}></div>
     </div>
   );
 };
